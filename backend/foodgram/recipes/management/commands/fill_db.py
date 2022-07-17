e895811451e96ef07295e5a6333ffe0efde85f36
Андрей Карасев <anderic@yandex.ru>
2022-07-17 18:44:47 +0300

    [lease work

--- backend/foodgram/recipes/management/commands/fill_db.py
+++ backend/foodgram/recipes/management/commands/fill_db.py
@@ -7,7 +7,7 @@ from recipes.models import Ingridients
 class Command(BaseCommand):
     def handle(self, *args, **options):
 
-        with open("../data/ingredients.json", "rb") as f:
+        with open("./ingredients.json", "rb") as f:
             data = json.load(f)
 
             for val in data:
